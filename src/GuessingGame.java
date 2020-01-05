f6189b1e6ba7b8e923376848736b141556b2499e
jmalue <jrod2416@gmail.com>
2020-01-04 22:06:41 -0500

    guessing-game rule 3 redo

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -10,22 +10,29 @@ public class GuessingGame {
 		
 		System.out.println("Pick a number");
 		int numberGuessed = input.nextInt();
+		
 		if(numberGuessed == 7) {
 			System.out.println("You won!");
+			System.exit(0);
 		}
 		else if(numberGuessed == 0){
-	System.out.println("Pick a number between 1 and 10.");
+	System.out.println("Pick a number between 1 and 10. You get one more pick.");
 }
-			else 
-				System.out.println("You lose");
-		
-		else if(numberGuessed != 7){
-			System.out.println("Try one more time");
-		
+			else {
+				System.out.println("You lose but try one more time.");
+	}
+		int numberGuessed1 = input.nextInt();
+		if(numberGuessed1 != 7)
+		{	System.out.println("You lose!");
+		}	
+		else if (numberGuessed1 == 7) {
+			System.out.println("You win!");
+		}
 			
 		
 		input.close();
 		
-	}
-
+		}
+	
 }
+
