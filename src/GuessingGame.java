d45f43f2f1f5ef4fabf1a3bef974f14feb630fa2
jmalue <jrod2416@gmail.com>
2020-01-05 10:12:09 -0500

    guessing-game attempted 1

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,47 +1,56 @@
-import java.util.Scanner;
+import java.util.Scanner; 
+
+import java.util.concurrent.ThreadLocalRandom; 
 
 public class GuessingGame {
 
-	public static void main(String[] args) { 
-		// TODO Auto-generated method stub
+	public static void main(String[] args)  {
 
-		
-		Scanner input = new Scanner(System.in);
+
+	
+Scanner input = new Scanner(System.in);
 		
 		System.out.println("Pick a number");
 		int numberGuessed = input.nextInt();
-		
-		if(numberGuessed == 7) {
+		int secretNum = ThreadLocalRandom.current().nextInt(1, 11); 
+		//did 11 instead of 10 to keep 10 inclusive
+		int attempts = 0;
+	
+		//loop
+
+         do {
+			if (numberGuessed == secretNum) { 
+				System.out.println("You win!!!");
+			}
+			if(numberGuessed == 7) { 
 			System.out.println("You won!");
+			System.exit(0); 
+			}
+			else if(numberGuessed == -1) {
 			System.exit(0);
-		}
-		else if(numberGuessed == -1){
-			System.exit(0);
-		}
-		else if(numberGuessed == 0){
+			}
+			else if(numberGuessed == 0) {
 	System.out.println("Pick a number between 1 and 10. You get one more pick.");
-        }
-		else if((numberGuessed >=1) && (numberGuessed <=6)) {
+			}
+		 else if((numberGuessed >=1) && (numberGuessed <=6)) {
 		     System.out.println("Secrect Number is > than number chosen.");
-	    }
-         else if((numberGuessed >=8) && (numberGuessed <=10)) {
+		 	}
+		   else if((numberGuessed >=8) && (numberGuessed <=10)) { 
 		     System.out.println("Secrect Number is < than number chosen.");
-         }
+		   }
 			else {
 				System.out.println("You lose but try one more time.");
-	}
-		int numberGuessed1 = input.nextInt();
-		if(numberGuessed1 != 7)
-		{	System.out.println("You lose!");
-		}	
-		else if (numberGuessed1 == 7) {
+			}
+			int numberGuessed1 = input.nextInt();
+			if(numberGuessed1 != 7) {
+			System.out.println("You lose!");
+			}
+			else if (numberGuessed1 == 7) {
 			System.out.println("You win!");
-		}
-			
+			} 
+         } while(attempts < 3);
+         
+		input.close(); 
 		
-		input.close();
-		
 		}
-	
 }
-
